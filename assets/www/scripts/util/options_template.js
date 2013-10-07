8f87392b51cb4001a0cfb5c10a63cec268162290
Peter Henderson <Breakend@Peters-MacBook-Pro.local>
2013-10-06 20:51:30 -0400

    Trying to port updated version to android with modules for storage
    First commit, dumping new assets. Cordova integration to come in a
    minute

---
+++ assets/www/scripts/util/options_template.js
@@ -0,0 +1,11 @@
+window.DEBUG = false;
+window.guest = "guest";
+window.showInLogin = false;
+window.showExpertOptions = false;
+window.DEV = {
+	disableMenu : true,
+	disableSplash : false,
+	disableMusic : false,
+	enableTableMode : false,
+	logging : false
+};
